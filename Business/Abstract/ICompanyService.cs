09a59a2f417ea5dde21ceec6176c7afa1555f68b
= <=>
2022-04-27 22:30:13 +0300

    Result yapısı oluşturuldu

---
+++ Business/Abstract/ICompanyService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Utilities.Results.Abstarct;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface ICompanyService
+    {
+        IResult Add(Companies companies);
+        IDataResult<List<Companies>> GetList();
+    }
+}
---
+++ Business/Concrete/CompanyManager.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Business.Constans;
+using Core.Utilities.Results.Abstarct;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CompanyManager : ICompanyService
+    {
+        private readonly ICompanyDal _companyDal;
+        public CompanyManager(ICompanyDal companyDal)
+        {
+            _companyDal = companyDal;
+        }
+
+        public IResult Add(Companies companies)
+        {
+            _companyDal.Add(companies);
+            return new SuccessResult(true,Messages.AddedCompany);
+        }
+
+        public IDataResult<List<Companies>> GetList()
+        {
+            return new SuccessDataResult<List<Companies>>(_companyDal.GetList());
+        }
+    }
+}
